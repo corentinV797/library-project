64f95275b5099311dbfb73465698008b8687b405
Corentin Vives <corentin.vives@viseo.com>
2021-03-29 17:43:37 +0200

    [feat] image storage

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { Book } from '../models/book.model';
 import firebase from 'firebase/app';
 import 'firebase/database';
+import 'firebase/storage';
 import DataSnapshot = firebase.database.DataSnapshot;
 
 @Injectable()
@@ -53,9 +54,20 @@ export class BooksService {
   }
 
   removeBook(book: Book) {
+    if(book.photo) {
+      const storageRef = firebase.storage().refFromURL(book.photo);
+      storageRef.delete().then(
+        () => {
+          console.log('Photo removed!');
+        },
+        (error) => {
+          console.log('Could not remove photo! : ' + error);
+        }
+      );
+    }
     const bookIndexToRemove = this.books.findIndex(
-      (bookE1) => {
-        if(bookE1 === book) {
+      (bookEl) => {
+        if(bookEl === book) {
           return true;
         } else {
           return false;
@@ -65,5 +77,27 @@ export class BooksService {
     this.books.splice(bookIndexToRemove, 1);
     this.saveBooks();
     this.emitBooks();
+  }
+
+  uploadFile(file: File) {
+    return new Promise<string>(
+      (resolve, reject) => {
+        const almostUniqueFileName = Date.now().toString();
+        const upload = firebase.storage().ref()
+          .child('images/' + almostUniqueFileName + file.name).put(file);
+        upload.on(firebase.storage.TaskEvent.STATE_CHANGED,
+          () => {
+            console.log('Chargement...');
+          },
+          (error) => {
+            console.log('Erreur de chargement ! : ' + error);
+            reject();
+          },
+          () => {
+            resolve(upload.snapshot.ref.getDownloadURL());
+          }
+        );
+      }
+    );
   }
 }
